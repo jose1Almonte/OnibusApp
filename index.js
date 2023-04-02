3ba45f1cd1e90e6b1c5b6ed5dc9aa76e8ea80932
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-04-02 14:22:13 -0400

    Aprendi sobre "AddRegistry"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { HomePage } from './pages/HomePage/HomePage';
 // import {NavigationContainer} from '@react-navigation/native';
 // import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -25,3 +26,5 @@ import {name as appName} from './app.json';
 // }
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => HomePage);
